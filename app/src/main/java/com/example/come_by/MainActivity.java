461bf5d868823e5f4a5989328ab748cef1f89ada
LateNightSeoul <60308568+LateNightSeoul@users.noreply.github.com>
2020-07-29 23:56:22 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/come_by/MainActivity.java
+++ app/src/main/java/com/example/come_by/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.come_by;
 
-
-
-
 import androidx.annotation.NonNull;
-import androidx.fragment.app.FragmentActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -13,7 +10,7 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
     private FragmentManager fm;
@@ -28,6 +25,13 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frag1 = new Frag1();
+        frag2 = new Frag2();
+        frag3 = new Frag3();
+        frag4 = new Frag4();
+        frag5 = new Frag5();
+        setFrag(0);
 
         bottomNavigationView = bottomNavigationView.findViewById(R.id.bottomNavi);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -55,12 +59,6 @@ public class MainActivity extends FragmentActivity {
             }
         });
 
-        frag1 = new Frag1();
-        frag2 = new Frag2();
-        frag3 = new Frag3();
-        frag4 = new Frag4();
-        frag5 = new Frag5();
-        setFrag(0);
 
     }
 
@@ -70,24 +68,29 @@ public class MainActivity extends FragmentActivity {
         switch (n) {
             case 0:
                 ft.replace(R.id.main_frame, frag1);
-                ft.addToBackStack(null);
+
                 ft.commit();
+                break;
             case 1:
                 ft.replace(R.id.main_frame, frag2);
-                ft.addToBackStack(null);
+
                 ft.commit();
+                break;
             case 2:
                 ft.replace(R.id.main_frame, frag3);
-                ft.addToBackStack(null);
+
                 ft.commit();
+                break;
             case 3:
                 ft.replace(R.id.main_frame, frag4);
-                ft.addToBackStack(null);
+
                 ft.commit();
+                break;
             case 4:
                 ft.replace(R.id.main_frame, frag5);
-                ft.addToBackStack(null);
+
                 ft.commit();
+                break;
         }
     }
 }
