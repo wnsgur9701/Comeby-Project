76519b6b86e021b1c269b5a52b62406f6514fedf
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2020-08-05 00:40:48 +0900

    이미지 슬라이더 자동넘기기
    오케이

--- app/src/main/java/com/example/come_by/Frag1.java
+++ app/src/main/java/com/example/come_by/Frag1.java
@@ -1,6 +1,7 @@
 package com.example.come_by;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +10,18 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class Frag1 extends Fragment {
 
     private View view;
     private ViewPager viewPager;
+    int currentPage = 0;
+    Timer timer;
+    final long DELAY_MS = 500;//delay in milliseconds before task is to be executed
+    final long PERIOD_MS = 3000; // time in milliseconds between successive task executions.
 
 
     @Nullable
@@ -24,6 +32,25 @@ public class Frag1 extends Fragment {
 
         viewPager = (ViewPager) view.findViewById(R.id.viewPager);
         viewPager.setAdapter(new ImageAdapter(getActivity()));
+
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            @Override
+            public void run() {
+                if(currentPage == 3) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, DELAY_MS, PERIOD_MS);
 
 
         return view;
