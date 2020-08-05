c2e4e450d5d47d1dc78361a4fe5e46d8ffd2fcb7
wnsgur9701 <62540133+wnsgur9701@users.noreply.github.com>
2020-08-05 15:25:34 +0900

    설명추가
    쓰레드에 대한 이해가 아직 부족하다..

--- app/src/main/java/com/example/come_by/Frag1.java
+++ app/src/main/java/com/example/come_by/Frag1.java
@@ -20,20 +20,21 @@ public class Frag1 extends Fragment {
     private ViewPager viewPager;
     int currentPage = 0;
     Timer timer;
-    final long DELAY_MS = 500;//delay in milliseconds before task is to be executed
-    final long PERIOD_MS = 3000; // time in milliseconds between successive task executions.
+    final long DELAY_MS = 500; //처음 대기 시간
+    final long PERIOD_MS = 3000; // 반복 시간, 즉 몇 초 마다 count+1이 되는지
 
-
+    //onCreate가 실행된 이후 onCreateView가 실행된다.
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.frag1, container, false);
+        view = inflater.inflate(R.layout.frag1, container, false);//container안에 R.layout.frag1이 삽입된다.
 
         viewPager = (ViewPager) view.findViewById(R.id.viewPager);
         viewPager.setAdapter(new ImageAdapter(getActivity()));
 
         final Handler handler = new Handler();
+        //runnable은 하나의 인터페이스로 규약이다. 하나라도 실행되지 않으면 안된다.
         final Runnable Update = new Runnable() {
             @Override
             public void run() {
@@ -51,6 +52,9 @@ public class Frag1 extends Fragment {
                 handler.post(Update);
             }
         }, DELAY_MS, PERIOD_MS);
+        //schedule(TimerTask task, long delay, long period)
+        //일정 시간(delay)이 지난 후에 일정 간격(period)으로 지정한 작업(task)을 수행한다.schedule(TimerTask task, long delay, long period)
+        //runnable 객체를 보낼 때는 handler.post를 이용한다.
 
 
         return view;
--- app/src/main/java/com/example/come_by/ImageAdapter.java
+++ app/src/main/java/com/example/come_by/ImageAdapter.java
@@ -22,18 +22,19 @@ public class ImageAdapter extends PagerAdapter {
     @Override
     public int getCount() {
         return mImageIds.length;
-    }
+    } // 전체 페이지 수
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return view == object;
+        return view == object; //해당 뷰와 오브젝트가 동일한 지 검사
     }
 
+    //화면에 표시할 페이지 뷰 생성
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         ImageView imageView = new ImageView(mContext);
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP); // ceter_crop ---> 이미지뷰에 크기에 맞게 꽉차게 이미지를 넣는다.
         imageView.setImageResource(mImageIds[position]);
         container.addView(imageView, 0);
         return imageView;
@@ -41,6 +42,6 @@ public class ImageAdapter extends PagerAdapter {
 
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        container.removeView((ImageView) object);
+        container.removeView((ImageView) object); //뷰페이져에서 삭제
     }
 }
